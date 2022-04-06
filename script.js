0800c03db3005ad054314f8bee01f4e061ecaf18
fihadranzer <rhfiha@gmail.com>
2022-04-06 13:00:15 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach( panel =>{
+    console.log(panel)
+
+    panel.addEventListener('click',()=>{
+        removeActive();
+        panel.classList.add('active');
+    })
+})
+
+
+
+function removeActive(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
+
